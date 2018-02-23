813be9de6a90b3f70ebe7ba25d1c78d38fadb159
zhss <zhss@163.com>
2018-02-23 12:06:16 +0800

    解决冲突

--- src/main/java/com/zhss/eshop/logistics/service/LogisticsFacadeService.java
+++ src/main/java/com/zhss/eshop/logistics/service/LogisticsFacadeService.java
@@ -1,10 +1,6 @@
 package com.zhss.eshop.logistics.service;
 
-<<<<<<< HEAD
 import com.zhss.eshop.commodity.dto.GoodsSkuDTO;
-=======
-import com.zhss.eshop.commodity.domain.GoodsSkuDTO;
->>>>>>> 23c8f5ae9e7c85e3cf2a8b2ec05b63b4a0e8d096
 
 /**
  * 物流中心对外提供的接口
