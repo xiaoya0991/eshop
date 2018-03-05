2fcf0bfe30889fee3145f1e8b453b93e807c08b9
zhss <zhss@163.com>
2018-03-05 10:51:34 +0800

    给wms中心对外接口service一个默认实现

--- src/main/java/com/zhss/eshop/wms/service/impl/WmsServiceImpl.java
+++ src/main/java/com/zhss/eshop/wms/service/impl/WmsServiceImpl.java
@@ -1,57 +1,73 @@
-package com.zhss.eshop.wms.service;
+package com.zhss.eshop.wms.service.impl;
+
+import org.springframework.stereotype.Service;
 
 import com.zhss.eshop.order.domain.OrderInfoDTO;
 import com.zhss.eshop.wms.domain.PurchaseInputOrderDTO;
 import com.zhss.eshop.wms.domain.ReturnGoodsInputOrderDTO;
 import com.zhss.eshop.wms.domain.SaleDeliveryOrderDTO;
+import com.zhss.eshop.wms.service.WmsService;
 
 /**
- * WMS中心对外提供的接口
+ * wms中心对外接口service组件
  * @author zhonghuashishan
  *
  */
-public interface WmsFacadeService {
-
+@Service
+public class WmsServiceImpl implements WmsService {
+	
 	/**
 	 * 创建采购入库单
 	 * @param purchaseInputOrderDTO 采购入库单DTO
 	 * @return 处理结果
 	 */
-	Boolean createPurchaseInputOrder(PurchaseInputOrderDTO purchaseInputOrderDTO);
+	public Boolean createPurchaseInputOrder(PurchaseInputOrderDTO purchaseInputOrderDTO) {
+		return true;
+	}
 	
 	/**
 	 * 创建销售出库单
 	 * @param saleDeliveryOrderDTO 销售出库单DTO
 	 * @return 处理结果
 	 */
-	Boolean createSaleDeliveryOrder(SaleDeliveryOrderDTO saleDeliveryOrderDTO);
+	public Boolean createSaleDeliveryOrder(SaleDeliveryOrderDTO saleDeliveryOrderDTO) {
+		return true;
+	}
 	
 	/**
 	 * 创建退货入库单
 	 * @param returnGoodsInputOrder 退货入库单DTO
 	 * @return 处理结果
 	 */
-	Boolean createReturnGoodsInputOrder(ReturnGoodsInputOrderDTO returnGoodsInputOrder);
+	public Boolean createReturnGoodsInputOrder(ReturnGoodsInputOrderDTO returnGoodsInputOrder) {
+		return true;
+	}
 	
 	/**
 	 * 通知WMS中心，“提交订单”事件发生了
 	 * @param orderDTO 订单DTO
 	 * @return 处理结果
 	 */
-	Boolean informSubmitOrderEvent(OrderInfoDTO orderDTO);
+	public Boolean informSubmitOrderEvent(OrderInfoDTO orderDTO) {
+		return true;
+	}
 	
 	/**
 	 * 通知WMS中心，“支付订单”事件发生了
 	 * @param orderDTO 订单DTO
 	 * @return 处理结果
 	 */
-	Boolean informPayOrderEvent(OrderInfoDTO orderDTO);
+	public Boolean informPayOrderEvent(OrderInfoDTO orderDTO) {
+		return true;
+	}
 	
 	/**
 	 * 通知WMS中心，“取消订单”事件发生了
 	 * @param orderDTO 订单DTO
 	 * @return 处理结果
 	 */
-	Boolean informCancelOrderEvent(OrderInfoDTO orderDTO);
-	
+	public Boolean informCancelOrderEvent(OrderInfoDTO orderDTO) {
+		return true;
+	}
+
 }
