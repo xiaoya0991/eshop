0e841c25dbca6288c0546edd0eec37df776ea175
zhss <zhss@163.com>
2018-03-07 20:15:02 +0800

    在物流中心的运费计算接口中增加收货地址参数

--- src/main/java/com/zhss/eshop/logistics/service/LogisticsService.java
+++ src/main/java/com/zhss/eshop/logistics/service/LogisticsService.java
@@ -1,6 +1,7 @@
 package com.zhss.eshop.logistics.service;
 
 import com.zhss.eshop.commodity.domain.GoodsSkuDTO;
+import com.zhss.eshop.membership.domain.DeliveryAddressDTO;
 
 /**
  * 物流中心对外提供的接口
@@ -14,6 +15,7 @@ public interface LogisticsService {
 	 * @param goodsSkuDTO 商品sku DTO
 	 * @return 商品sku的运费
 	 */
-	Double calculateFreight(GoodsSkuDTO goodsSkuDTO);
+	Double calculateFreight(GoodsSkuDTO goodsSkuDTO, 
+			DeliveryAddressDTO deliveryAddress);
 	
 }
