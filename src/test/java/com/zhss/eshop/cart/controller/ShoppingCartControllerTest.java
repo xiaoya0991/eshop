2908019c04f4ce40040caa1c7b8cc43160db1870
zhss <zhss@163.com>
2018-03-31 19:39:22 +0800

    调试集成测试环境启动

--- src/test/java/com/zhss/eshop/cart/controller/ShoppingCartControllerTest.java
+++ src/test/java/com/zhss/eshop/cart/controller/ShoppingCartControllerTest.java
@@ -1,6 +1,5 @@
 package com.zhss.eshop.cart.controller;
 
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
